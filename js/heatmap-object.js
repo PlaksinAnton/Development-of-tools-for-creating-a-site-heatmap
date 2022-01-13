a68b480cefc77130e1b67b35d32249ceca4f8597
avsyurakshina1 <avsyurakshina@miem.hse.ru>
2022-01-13 15:49:54 +0300

    update: heatmap-object.js
    realize simple work of mouse-tracker

--- js/heatmap-object.js
+++ js/heatmap-object.js
@@ -1,22 +1,24 @@
-// create configuration object
-var config = {
-  container: document.getElementById('heatmapContainer'),
-  radius: 10,
-  maxOpacity: .5,
-  minOpacity: 0,
-  blur: .75
+window.onload = function () {
+  var heatmapContainer = document.querySelector('.heatmap');
+
+  var heatmapInstance = h337.create({
+    container: heatmapContainer,
+    radius: 50
+  });
+
+  heatmapContainer.onmousemove = heatmapContainer.ontouchmove = function (e) {
+    // we need preventDefault for the touchmove
+    e.preventDefault();
+    var x = e.layerX;
+    var y = e.layerY;
+    if (e.touches) {
+      x = e.touches[0].pageX;
+      y = e.touches[0].pageY;
+    }
+
+    heatmapInstance.addData({ x: x, y: y, value: 1 });
+  };
+  heatmapContainer.onclick = function (ev) {
+    heatmapInstance.addData({ x: ev.layerX, y: ev.layerY, value: 1 });
+  };
 };
-// create heatmap with configuration
-var heatmapInstance = h337.create(config);
-
-// // a single datapoint
-// var dataPoint = {
-//   x: 5, // x coordinate of the datapoint, a number
-//   y: 5, // y coordinate of the datapoint, a number
-//   value: 100 // the value at datapoint(x, y)
-// };
-// heatmapInstance.addData(dataPoint);
-
-// // multiple datapoints (for data initialization use setData!!)
-// var dataPoints = [dataPoint, dataPoint, dataPoint, dataPoint];
-// heatmapInstance.addData(dataPoints);
