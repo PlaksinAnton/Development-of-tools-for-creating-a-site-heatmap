db17a62034797572a58c81c7f1cf3b92ecb734e5
xomyak-coder <dkhol@inbox.ru>
2022-03-04 19:36:05 +0300

    start to do graph-page

---
+++ front-end/src/pages/GraphicPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GraphContainer from "../components/GraphContainer.jsx";
+
+
+const GraphicPage = (props) => {
+    return (
+        <>
+            <GraphContainer/>
+        </>
+    )
+}
+export { GraphicPage };
