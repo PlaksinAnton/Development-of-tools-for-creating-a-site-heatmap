5b283a808a9f4c941da6b527ab90711399d8281d
xomyak-coder <dkhol@inbox.ru>
2022-04-12 21:38:27 +0300

    рабочая версия

--- front-end/src/pages/GraphicPage.jsx
+++ front-end/src/pages/GraphicPage.jsx
@@ -1,13 +1,6 @@
 import React, { useState, useEffect } from "react";
 import GraphContainer from "../components/GraphContainer.jsx";
-// import h337 from "heatmap.js";
-import detect from "detect.js";
-import axios from "axios";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
-import { getPosition } from "../Clicks";
 import AdminHeader from "../components/adminHeader.jsx";
-import AdminFooter from "../components/adminFooter";
 
 function GraphicPage() {
   return (
